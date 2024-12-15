d924bef7deebb4adac1f84ebcb76393d51827d0a
dodgeja3 <zambezi64@gmail.com>
2024-12-15 16:41:08 -0500

    Enhance background, sounds, rotations
    -better background assets
    - flap, death sounds
    - fix flappy rotation

--- scenes/death_manager.gd
+++ scenes/death_manager.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
 @onready var dead_label = $DeadLabel
-@onready var timer = $Timer
+@onready var timer = $RestartTimer
+@onready var death_tone = $DeathTone
 
 func die():
 	# If we're already dead, return
-	if !$Timer.is_stopped():
+	if !timer.is_stopped():
 		return
 	var flappy = get_tree().get_first_node_in_group("player_group") as Node2D
 	dead_label.visible = true
 	dead_label.global_position = flappy.global_position
+	death_tone.play()
+	
 	timer.start()
-	Engine.time_scale = 0.5
+	Engine.time_scale = 0.33
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
--- scenes/flappy/flappy.gd
+++ scenes/flappy/flappy.gd
@@ -9,6 +9,7 @@ const GRAVITY_SCALE = 0.30
 @onready var collision_shape = $CollisionShape2D
 @onready var animation_player = $AnimationPlayer
 @onready var flap_audio = $AudioStreamPlayer2D
+@onready var hurt_box = $HurtBox
 @onready var is_dead = false
 
 func _physics_process(delta: float) -> void:
@@ -23,14 +24,15 @@ func _physics_process(delta: float) -> void:
 	# Play flap animation
 	if $JumpTimer.is_stopped():
 		animated_sprite.play("default")
-	else:
-		animated_sprite.play("flap")
 		
 		
 	# Only jump if allowed
 	if Input.is_action_just_pressed("jump") and $JumpTimer.is_stopped():
 		velocity.y = JUMP_VELOCITY
 		$JumpTimer.start()
+		flap_audio.play()
+		animation_player.stop(true)
+		animation_player.play("flap")
 
 	# Go fast and RIIIIGHT
 	velocity.x = 1 * SPEED
@@ -38,6 +40,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func die():
-	print("You Died!")
 	animation_player.play("die")
+	# Disable collision shape to avoid hitting other things when dead
+	collision_shape.set_deferred("disabled", true)
 	is_dead = true
